8db98d930dfbb2d98a81760c3b90dbbcf0272c83
anushasima <0939713580@ukr.net>
2025-01-19 15:54:34 +0200

    added fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 elements = random.randint(3, 10)
 random_list = [random.randint(1, 100) for _ in range(elements)]
 print(random_list)
-new_list = random_list[:1] + random_list[2:3] + random_list[-2:-1]
+new_list = [random_list[0], random_list[2], random_list[-2]]
 print(new_list)
 
 # my_list = [1, 2, 3, 4, 5, 6, 7, 9]
